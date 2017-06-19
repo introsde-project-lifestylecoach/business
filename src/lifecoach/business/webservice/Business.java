b0b1c748db8d7107a0062ee354408ff0fcd85b84
ABrun93 <a.brun93@gmail.com>
2017-06-19 11:57:12 +0200

    Base project with create, update and delete

---
+++ src/lifecoach/business/webservice/Business.java
@@ -0,0 +1,63 @@
+package lifecoach.business.webservice;
+
+import lifecoach.localdb.webservice.Person;
+import lifecoach.localdb.webservice.Measure;
+import lifecoach.localdb.webservice.Goal;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.WebResult;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
+
+@WebService
+@SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL) //optional
+public interface Business 
+{
+	/* Person */
+
+    @WebMethod(operationName="createPerson")
+    @WebResult(name="personId") 
+    public int addPerson(@WebParam(name="person", targetNamespace="http://webservice.storage.lifecoach/") Person person);
+
+    @WebMethod(operationName="updatePerson")
+    @WebResult(name="personId") 
+    public int updatePerson(@WebParam(name="person", targetNamespace="http://webservice.storage.lifecoach/") Person person);
+
+    @WebMethod(operationName="deletePerson")
+    @WebResult(name="result") 
+    public int deletePerson(@WebParam(name="personId") int id);
+    
+    
+    /* Measure */
+    
+    @WebMethod(operationName="createMeasure")
+    @WebResult(name="measureId") 
+    public int addMeasure(@WebParam(name="personId") int pId, @WebParam(name="measure", targetNamespace="http://webservice.localdb.lifecoach/") Measure measure);
+
+    @WebMethod(operationName="updateMeasure")
+    @WebResult(name="measureId") 
+    public int updateMeasure(@WebParam(name="personId") int pId, @WebParam(name="measure", targetNamespace="http://webservice.localdb.lifecoach/") Measure measure);
+    
+    @WebMethod(operationName="deleteMeasure")
+    @WebResult(name="result") 
+    public int deleteMeasure(@WebParam(name="measureId") int id);
+       
+    
+    /* Goal */
+    
+    @WebMethod(operationName="createGoal")
+    @WebResult(name="goalId") 
+    public int addGoal(@WebParam(name="personId") int pId, @WebParam(name="goal", targetNamespace="http://webservice.localdb.lifecoach/") Goal goal);
+
+    @WebMethod(operationName="updateGoal")
+    @WebResult(name="goalId") 
+    public int updateGoal(@WebParam(name="personId") int pId, @WebParam(name="goal", targetNamespace="http://webservice.localdb.lifecoach/") Goal goal);
+    
+    @WebMethod(operationName="deleteGoal")
+    @WebResult(name="result") 
+    public int deleteGoal(@WebParam(name="goalId") int id);
+    
+}
---
+++ src/lifecoach/business/webservice/BusinessImplementation.java
@@ -0,0 +1,122 @@
+package lifecoach.business.webservice;
+
+import lifecoach.storage.webservice.StorageService;
+import lifecoach.storage.webservice.Storage;
+import lifecoach.localdb.webservice.Person;
+import lifecoach.localdb.webservice.Measure;
+import lifecoach.localdb.webservice.Goal;
+
+import javax.jws.WebService;
+
+//Service Implementation
+
+@WebService(endpointInterface = "lifecoach.business.webservice.Business",
+    serviceName="BusinessService")
+public class BusinessImplementation implements Business 
+{
+	StorageService service;
+	Storage storage;
+	
+	public void init()
+	{
+		service = new StorageService();
+        storage = service.getStorageImplementationPort();
+	}
+	
+	
+	/* Manage Person*/
+
+    @Override
+    public int addPerson(Person person) {
+    	init();
+    	System.out.println("Save Person with id = " + person.getIdPerson());
+    	return storage.createPerson(person);
+    }
+
+    @Override
+    public int updatePerson(Person person) {
+    	init();
+    	if(person == null)
+    	{
+    		System.out.println("Zio billy");
+    		return 1;
+    	}    	
+    	System.out.println("Update Person with id = " + person.getIdPerson());
+        return storage.updatePerson(person);
+    }
+
+    @Override
+    public int deletePerson(int id) {
+    	init();
+    	System.out.println("Delete Person with id = " + id);           
+        return storage.deletePerson(id);
+    }
+    
+    
+    /* Manage Measure */
+
+    @Override
+    public int addMeasure(int pId, Measure measure) {
+    	init();
+    	System.out.println("Save Measure with id = " + measure.getIdMeasure());
+        
+    	if(measure.getMeasureType()==null)
+    	{
+    		System.out.println("QUi");
+    	}
+    	else
+    	{
+    		System.out.println("QUa " + measure.getMeasureType().getType());
+    	}
+    	
+        return storage.createMeasure(pId, measure);
+    }
+
+    @Override
+    public int updateMeasure(int pId, Measure measure) {
+    	init();
+    	System.out.println("Update Measure with id = " + measure.getIdMeasure());  	
+    	return storage.updateMeasure(pId, measure);
+    }
+    
+    @Override
+    public int deleteMeasure(int id) {
+    	init();
+    	System.out.println("Delete Measure with id = " + id);
+    	return storage.deleteMeasure(id);
+    }
+    
+    
+    /* Manage MeasureType */
+       
+    @Override
+    public int addGoal(int pId, Goal goal) {
+    	init();
+    	System.out.println("Save Goal with id = " + goal.getIdGoal());
+        
+    	if(goal.getMeasureType()==null)
+    	{
+    		System.out.println("QUi");
+    	}
+    	else
+    	{
+    		System.out.println("QUa " + goal.getMeasureType().getType());
+    	}
+    	
+    	return storage.createGoal(pId, goal);
+    }
+
+    @Override
+    public int updateGoal(int pId, Goal goal) {
+    	init();
+    	System.out.println("Update Goal with id = " + goal.getIdGoal());
+    	return storage.updateGoal(pId, goal);
+    }
+    
+    @Override
+    public int deleteGoal(int id) {
+    	init();
+    	System.out.println("Delete Goal with id = " + id);
+    	return storage.deleteGoal(id);
+    }
+}
